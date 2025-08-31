16b397b0e624e846da74a04ee17398ae8f501a14
Eddie Rivera <rivera_fx2@yahoo.com>
2025-08-30 20:45:36 -0400

    Added git lfs support and updated shell files to have chmod +x

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 apt update
 apt install xorg -y
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt install -y git-lfs
 bash /workspace/scripts/blender_install.sh
 echo "Done installing. Ready to render with ./render_frame.sh or ./render_anim.sh"
