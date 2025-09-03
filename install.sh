020abb8d125e916ad4e242118b5769310f9e069f
Eddie Rivera <rivera_fx2@yahoo.com>
2025-09-03 00:26:20 -0400

    Added support for python pandas.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ apt install nano -y
 apt install xorg -y
 #curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 pip install dropbox
+pip install pandas
 bash /workspace/scripts/blender_install.sh
 echo ""
 echo "Done installing. To download blend files use ./render_download.sh"
