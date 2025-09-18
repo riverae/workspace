ed8f41cb0bb65ae01e5fe7e9824bc8c6d5cc5d5f
Eddie Rivera <rivera_fx2@yahoo.com>
2025-09-18 02:12:43 -0400

    Fixed variable in string for render path.

--- render_easystates.sh
+++ render_easystates.sh
@@ -11,5 +11,5 @@ if [ ! -d "$renders_path/$dir" ]; then
   mkdir -p "$renders_path/$dir"
 fi
 
-echo /opt/blender_4.5/blender -b /workspace/projects/*.blend -P /workspace/scripts/setgpu.py --python-expr 'import bpy; bpy.context.scene.easystates_manager.render_output="/workspace/renders/"$dir"/"; bpy.ops.easystates.background_render()' -F OPEN_EXR_MULTILAYER
-/opt/blender_4.5/blender -b /workspace/projects/*.blend -P /workspace/scripts/setgpu.py --python-expr 'import bpy; bpy.context.scene.easystates_manager.render_output="/workspace/renders/"$dir"/"; bpy.ops.easystates.background_render()' -F OPEN_EXR_MULTILAYER
+echo /opt/blender_4.5/blender -b /workspace/projects/*.blend -P /workspace/scripts/setgpu.py --python-expr 'import bpy; bpy.context.scene.easystates_manager.render_output="/workspace/renders/'$dir'/"; bpy.ops.easystates.background_render()' -F OPEN_EXR_MULTILAYER
+/opt/blender_4.5/blender -b /workspace/projects/*.blend -P /workspace/scripts/setgpu.py --python-expr 'import bpy; bpy.context.scene.easystates_manager.render_output="/workspace/renders/'$dir'/"; bpy.ops.easystates.background_render()' -F OPEN_EXR_MULTILAYER
