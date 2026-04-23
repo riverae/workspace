9f77bc89628eecb8c0197db69ec0637830f9ed64
riverae <riverae@users.noreply.github.com>
2026-04-23 15:08:55 -0400

    Update Blender version in render_frame.sh script

--- render_frame.sh
+++ render_frame.sh
@@ -2,14 +2,14 @@
 # create a string from the value of arguments
 # iterate over each argument and build a string variable
 
-
+BLENDER_VERSION=5.1.1
    
 if [ -z "$1" ]; then
-  echo /opt/blender_4.5/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py -f 1
-  /opt/blender_4.5/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py -f 1
+  echo /opt/blender_${BLENDER_VERSION%.*}/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py -f 1
+  /opt/blender_${BLENDER_VERSION%.*}/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py -f 1
 else
   array=( "$@" )
   items=$(printf " -f %s" "${array[@]}")
-  echo /opt/blender_4.5/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py${items[@]}
-  /opt/blender_4.5/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py${items[@]}
+  echo /opt/blender_${BLENDER_VERSION%.*}/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py${items[@]}
+  /opt/blender_${BLENDER_VERSION%.*}/blender -b /workspace/projects/*.blend -o /workspace/renders/output/ -P /workspace/scripts/setgpu.py${items[@]}
 fi
