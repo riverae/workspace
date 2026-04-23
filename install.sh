114b7adc8d2610565f8bdfaf7a4ea77c2c2abcdf
riverae <riverae@users.noreply.github.com>
2026-04-23 14:52:35 -0400

    Comment out addons installation script call
    Comment out the call to addons_install.sh in the installation script.

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ pip install dropbox
 pip install pandas
 pip install tqdm
 bash /workspace/scripts/blender_install.sh
-bash /workspace/scripts/addons_install.sh
+#bash /workspace/scripts/addons_install.sh
 cat /workspace/scripts/motionhive.txt
 echo "Done installing. To download blend files use ./render_download.sh"
 echo "Ready to render with ./render_easystates.sh, ./render_frame.sh or ./render_anim.sh"
